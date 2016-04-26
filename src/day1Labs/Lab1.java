edf06650ae9b8afba778ddc3f032eff50dff1e2f
Jason Scott <becomingcoder78@gmail.com>
2016-04-26 17:16:27 -0400

    Refactored code to move methods to class file

--- src/day1Labs/Lab1.java
+++ src/day1Labs/Lab1.java
@@ -26,7 +26,7 @@ public class Lab1 {
 		
 		while (choice.equalsIgnoreCase("y")) {
 			
-			areaHeightVolume(sc);
+			Volume.areaHeightVolume(sc);
 			
 			
 			// Ask if user wants to continue
@@ -39,47 +39,6 @@ public class Lab1 {
 
 	}
 
-	public static void areaHeightVolume(Scanner sc) {
-		float theLength;
-		float theWidth;
-		float theHeight;
-		
-		// User inputs Length
-		System.out.println("Enter Length:    ");
-		theLength = Float.parseFloat(sc.nextLine());
-		
-		// User inputs Width
-		System.out.println("Enter Width:    ");
-		theWidth = Float.parseFloat(sc.nextLine());
-		
-		// User inputs Height
-		System.out.println("Enter Height:    ");
-		theHeight = Float.parseFloat(sc.nextLine());
-		
-		// Calculate Area
-		float theArea = 2 * (theLength * theWidth);
-		
-		// Calculate Perimeter
-		float thePerimeter = theLength * theWidth;
-		
-		// Calculate Volume
-		float theVolume = theLength * theWidth * theHeight;
-		
-		
-		// Formatting the output to display only 2 digits
-		NumberFormat formatValue = NumberFormat.getInstance();
-		formatValue.setMaximumFractionDigits(2);
-		
-		// Setting the formatted variables to replace user input variables
-		String formatArea = formatValue.format(theArea);
-		String formatPerimeter = formatValue.format(thePerimeter);
-		String formatVolume = formatValue.format(theVolume);
 
-		
-		// Display the Area, Perimeter & Volume to user
-		System.out.println("Area:    " + formatArea);
-		System.out.println("Perimeter:    " + formatPerimeter);
-		System.out.println("Volume:       " + formatVolume);
-	}
 
 }
